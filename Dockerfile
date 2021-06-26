996b689cb8fb4361bfa909764669c3e1dd5455a8
kewlate2307 <kewlate2307@gmail.com>
2021-06-26 21:10:16 +0700

    visits_jenkin

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:14.17.0-alpine3.11
+
+WORKDIR '/app'
+
+COPY package.json .
+RUN npm install
+COPY . .
+
+CMD ["npm","start"]
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const redis = require('redis');
+const process = require('process');
+
+const app = express();
+const client = redis.createClient({
+  host: 'redis-server',
+  port: 6379,
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  
+  client.get('visits', (err, visits) => {
+    res.send('Number of visits ' + visits);
+    client.set('visits', parseInt(visits) + 1);
+    if(parseInt(visits) == 5){
+      process.exit(20);
+    }
+    
+  });
+});
+
+app.listen(8081, () => {
+  console.log('listening on port 8081');
+});
